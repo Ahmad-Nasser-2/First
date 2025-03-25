8f59f43c3c15dd7cb9c0c8ee5594097da61ae41f
Farah <hamrahacker@gmail.com>
2025-03-26 01:27:15 +0200

    Updated home addded

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,10 @@
 export default function Home() {
-
-    return <>
-
-    <h1 className="text-3xl font-bold underline text-center my-8 text-blue-600">Hello world </h1>
-
-    </>
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-center my-8 text-blue-600">
+                Hello world{" "}
+            </h1>
+            <h2> added </h2>
+        </>
+    );
 }

c6eb8564c931ddfb3b4e9cfc6f171cea71e43541
Farah <hamrahacker@gmail.com>
2025-03-28 12:41:45 +0200

    modified signuplogin syle

--- resources/js/Pages/Items.jsx
+++ resources/js/Pages/Items.jsx
@@ -110,7 +110,7 @@ const Items = () => {
                 <div className="mx-auto w-2/3 sm:w-full">
                     <CustomSelect label="Categories" options={options} />
                 </div>
-                <div>
+                <div className="mx-auto w-2/3 sm:w-full">
                     <button
                         type="button"
                         class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
